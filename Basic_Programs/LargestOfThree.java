49584f001155ca7daabfce1943c24be6d6f78fda
mohd2317 <154823790+mohd2317@users.noreply.github.com>
2025-08-03 09:12:45 +0530

    LargestOfThree
    Day 3 Basic Programs

---
+++ Basic_Programs/LargestOfThree.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        int largest = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println("Largest number is: " + largest);
+
+        sc.close();
+    }
+}
+
+// Time Complexity:O(1)
+// Space Complexity:O(1)
