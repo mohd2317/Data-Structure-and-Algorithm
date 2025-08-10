c3020680491deffab84af7f0b804d2e3bedc9ba1
Bilal <bilalmalik002345@gmail.com>
2025-08-10 15:57:56 +0530

    Day 5 Added some Programs in Arrays folder.

--- DSA/Arrays_Programs/ArrEqualityCheck.java
+++ DSA/Arrays_Programs/ArrEqualityCheck.java
@@ -1,19 +1,18 @@
+package Arrays_Programs;
+
 import java.util.Arrays;
 
 public class ArrEqualityCheck {
     public static void main(String[] args) {
+        int[] a = {10, 20, 30};
+        int[] b = {10, 20, 30};
 
-        int a[] = {10, 20, 30};
-        int b[] = {10, 20, 30};
-
-        // Using Arrays.equals() to check 
-        // if two arrays are equal
-        boolean equal = Arrays.equals(a, b);
-
-        if (equal) {
-            System.out.println("Equal");
-        } else {
-            System.out.println("Not Equal");
-        }
+        // Java 7 style
+        boolean equalJava7 = Arrays.equals(a, b);
+        System.out.println("(Java 7) Arrays Equal? " + equalJava7);
     }
 }
+
+//Java 7 way: Arrays.equals(a, b)
+//
+//Java 8 way: Uses Streams with allMatch() and anyMatch() to compare elements.
